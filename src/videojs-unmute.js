e87fb70a957091a600481bc90aac5b116a914845
Gino <ginocote@gmail.com>
2022-10-12 23:07:14 -0400

    Update src folder files

--- src/videojs-unmute.js
+++ src/videojs-unmute.js
@@ -1,3 +1,41 @@
+// LANGAGE TRANSLATE JSON
+const translate = {
+  "en": "UNMUTE",
+  "zh": "激活声音",
+  "hi": "ध्वनि सक्रिय करें",
+  "es": "ACTIVAR SONIDO",
+  "fr": "ACTIVER LE SON",
+  "ar": "تنشيط الصوت",
+  "bn": "নিঃশব্দ",
+  "ru": "ВКЛЮЧИТЬ ЗВУК",
+  "pt": "ATIVAR SOM",
+  "de": "TON AKTIVIEREN",
+  "id": "AKTIFKAN SUARA"
+};
+
+// GET USER BROWSER LANGAGE
+let lang = window.navigator.languages ? window.navigator.languages[0] : null;
+lang = lang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
+
+// lang = "de"; // TEST
+
+if (typeof lang === "undefined" || lang === null) lang = "us-en";
+
+let shortLang = lang;
+if (shortLang.indexOf("-") !== -1) shortLang = shortLang.split("-")[0];
+if (shortLang.indexOf("_") !== -1) shortLang = shortLang.split("_")[0];
+
+console.log(lang, shortLang);
+
+if (typeof translate[shortLang] === "undefined") {
+  console.log('Variable "shortLang" is undefined.');
+  translated = translated;
+} else {
+  console.log('Variable "shortLang": '+translate[shortLang]);
+  translated = translate[shortLang];
+}
+
+
 // Cross-compatibility for Video.js 5 and 6.
 const registerPlugin = videojs.registerPlugin || videojs.plugin;
 // const dom = videojs.dom || videojs;const registerPlugin = videojs.registerPlugin || videojs.plugin;
