2192a46971f6e169f28811cdc9b4c7710764b455
Gino <ginocote@gmail.com>
2022-10-25 16:28:35 -0400

    Keep some backup old files

--- backups/videojs-unmute.js
+++ backups/videojs-unmute.js
@@ -1,6 +1,6 @@
-This looks like a JavaScript file. Click this bar to format it.No 1
 // POSSIBLE OPTIONS TO ADD?
 // BUTTON LEFT RIGHT TOP BOTTOM - BUTTON COLOR WHITE OR BLACK -
+
 // LANGAGE TRANSLATE JSON
 const translate = {
   en: "UNMUTE",
@@ -15,6 +15,7 @@ const translate = {
   de: "TON AKTIVIEREN",
   id: "AKTIFKAN SUARA"
 };
+
 // GET USER BROWSER LANGAGE
 let lang = window.navigator.languages ? window.navigator.languages[0] : null;
 lang =
@@ -22,12 +23,17 @@ lang =
   window.navigator.language ||
   window.navigator.browserLanguage ||
   window.navigator.userLanguage;
+
 // lang = "de"; // TEST
+
 if (typeof lang === "undefined" || lang === null) lang = "us-en";
+
 let shortLang = lang;
 if (shortLang.indexOf("-") !== -1) shortLang = shortLang.split("-")[0];
 if (shortLang.indexOf("_") !== -1) shortLang = shortLang.split("_")[0];
+
 console.log(lang, shortLang);
+
 if (typeof translate[shortLang] === "undefined") {
   console.log('Variable "shortLang" is undefined.');
   translated = "UNMUTE";
@@ -35,13 +41,16 @@ if (typeof translate[shortLang] === "undefined") {
   console.log('Variable "shortLang": ' + translate[shortLang]);
   translated = translate[shortLang];
 }
+
 // for(shortLang of Object.keys(translate)){
 // // console.log(shortLang);
 //   console.log(translate[shortLang])
 // }
+
 // Object.keys(translate).forEach(function(key) {
 //   // console.log('translate : ' + key + ', Value : ' + translate[key])
 // })
+
 // if (translate.hasOwnProperty(shortLang)) {
 //   console.log(true);
 //   shortLang = translate[shortLang];
@@ -49,102 +58,236 @@ if (typeof translate[shortLang] === "undefined") {
 //   console.log(false);
 //   shortLang = en;
 // }
+
 // Cross-compatibility for Video.js 5 and 6.
 const registerPlugin = videojs.registerPlugin || videojs.plugin;
 // const dom = videojs.dom || videojs;const registerPlugin = videojs.registerPlugin || videojs.plugin;
+
 // videojs.registerPlugin("unmuteButton", function () { // VIDEOJS 7
-registerPlugin("unmuteButton", function() {
+
+registerPlugin("unmuteButton", function () {
   // Cross-compatibility for Video.js 5 and 6.
   let player = this;
-  let isUnmuted = false;
+
+  let isMuted = false;
   let hasPlayed = false;
-  let hasOverlay = false;
+  let haveOverlay = false;
   let allevents = false;
-  let setVol = player.volume();
-  console.log(setVol);
-  // let volumeLevel = setVol
-  let volumeLevel = 1;
-  //  
-  //  if (player.autoplay()) {
-  //    player.muted(true);
-  //    // alert(player.muted)
-  //  } else {
-  //    return false;
-  //  }
+  let isWrapped = false;
+
+  //  let setVol = player.volume();
+  //  console.log(setVol);
+  //  // let volumeLevel = setVol
+  //  let volumeLevel = 1; 
+
+
+  const thisplayerid = player.id();
+  const playerid = document.getElementById(thisplayerid);
+
+  if (!isWrapped) {
+
+    // create wrapper container
+    let wrapper = document.createElement('div');
+    wrapper.classList.add("vjs-wrapper");
+    // insert wrapper before el in the DOM tree
+    playerid.parentNode.insertBefore(wrapper, playerid);
+    // move el into wrapper
+    wrapper.appendChild(playerid);
+
+    isWrapped = true;
+
+  }
+
+  //    alert(player.currentType())
+
+  /******* CHECK IF AUTOPLAY *******/
+  if (player.autoplay()) {
+    player.muted(true);
+    isMuted = true;
+    //      haveOverlay = true;
+  } else {
+    return false;
+  }
+
+
+
+  /******************** CAN AUTOPLAY ? ***********************/
+  
+//  let promiseTEST = player.play();
+
+//  player.ready(function () {
+//    var promiseTEST = player.play();
+//
+//    if (promiseTEST !== undefined) {
+//      promiseTEST.then(function () {
+//        alert("Autoplay started!");
+//      }).catch(function (error) {
+//        alert("Autoplay was prevented!");
+//      });
+//    }
+//  });
+  
+  /******************** CAN AUTOPLAY ? END ***********************/ 
+  
+//    startPlayPromise
+//      .then(function () {
+//        // alert('CONSOLE2: Autoplay started!')
+//        console.log("CONSOLE2: Autoplay started!");
+//        // Autoplay started!
+//      })
+//      .catch(function (error) {
+//        console.log("CONSOLE2 Autoplay was prevented!");
+//        // Autoplay was prevented.
+//      });
+
+
+
   // +++ Wait for loadedmetadata then try to play video +++
-  player.ready(function() {
-    // +++ Wait for loadedmetadata then try to play video +++
-    // player.on("loadedmetadata", function () {
-    if (player.autoplay()) {
-      player.muted(true);
-      // alert(player.muted)
-    } else {
-      return false;
+  player.ready(function () {
+    
+    let promiseTEST = player.play();
+    
+    if (promiseTEST !== undefined) {
+      
+//      promiseTEST;
+      
+      promiseTEST.then(function () {
+        console.log("Autoplay started!");
+      }).catch(function (error) {
+        console.log("Autoplay was prevented!");
+      });
     }
+
+
     // Play video which returns a promise
     let startPlayPromise = player.play();
-    let thisplayerid = player.id();
-    player.on("play", function() {
+    //    let thisplayerid = player.id();
+
+    player.on("play", function () {
       hasPlayed = true;
-      console.log("hasOverlay: " + hasPlayed)
+      console.log("haveOverlay: " + haveOverlay)
     });
+
     // alert(thisplayerid)
+
     if (startPlayPromise !== undefined && player.autoplay()) {
       // let thisplayerid = player.id();
+
       startPlayPromise;
       // .then(() => {
-      player.on("loadedmetadata", function() {
+      player.on("loadedmetadata", function () {
         // loadedmetadata - FIX WHEN VIDEO DO NOT AUTOPLAY ERROR TO NOT SHOW UNMUTE BUTTON END GETTING THE PLAYER ID
+
         console.log(player);
+
         // let thisplayerid = player.id();
         // let thisplayerid = player.id_;
         console.log("playerid " + thisplayerid);
-        // IF hasOverlay AND AUTOPLAY WITH SOUND IT MEAN THE OVERLAY SHOULD NOT BE THERE AND DO NOT CREATE OVERLAY AGAIN AND RETURN FALSE
-        if (hasOverlay)
+
+        // IF haveOverlay AND AUTOPLAY WITH SOUND IT MEAN THE OVERLAY SHOULD NOT BE THERE AND DO NOT CREATE OVERLAY AGAIN AND RETURN FALSE
+        if (haveOverlay)
           return false;
-        const playerid = document.getElementById(player.id());
+        else
+          haveOverlay = true;
+
+        //        const playerid = document.getElementById(player.id());
         let divOverlay = document.createElement("div");
+
         // +++ Add button's event listener +++
-        divOverlay.addEventListener("click", function() {
+        divOverlay.addEventListener("click", function () {
           player.muted(false);
-          player.volume(volumeLevel);
+          isMuted = false;
+          player.load();
+          player.play();
+          player.volume(1);
           playerid.removeChild(divOverlay);
-          isUnmuted = true;
-          hasOverlay = false;
+          haveOverlay = false;
         });
-        console.log("isUnmuted: " + isUnmuted);
-        console.log("hasOverlay: " + hasOverlay)
-        // IF ISUNMUTED BUTON OVERLAY WORKED, REMOVE OVERLAY AND DO NOT CONTINUE RETURN FALSE
-        if (isUnmuted) {
-          if (hasOverlay) {
+
+        let vjsLoading = document.querySelector('.vjs-loading-spinner');
+        // +++ Add button's event listener +++
+        vjsLoading.addEventListener("click", function () {
+          player.muted(false);
+          player.volume(1);
+          player.load();
+          player.play();
+
+          if (haveOverlay);
+          playerid.removeChild(divOverlay);
+
+          isMuted = false;
+          haveOverlay = false;
+        });
+
+
+        /******* CHECK IF IS PLAYING BUT THE OVERLAY BUTTON IS STILL THERE *******/
+        player.on('playing', function () {
+          //          isPlaying = true;
+          if (!player.muted() && hasPlayed && haveOverlay) {
             playerid.removeChild(divOverlay);
-            hasOverlay = false;
+            haveOverlay = false;
+            isMuted = false;
+            // alert(plhaveOverlay)
           }
+        });
+
+        /*** ONEND THE USER SHOULD DO A INTERACTION TO PLAY THE VIDEO AGAIN, IF IT WAS UNMUTED THE OVERLAY BUTTON WILL BE REMOVED ***/
+        player.on('ended', function () {
+
+          //          alert("haveOverlay: " +haveOverlay );
+          console.log(isMuted);
+          if (player.muted() && haveOverlay && hasPlayed) {
+            console.log("on ended remove divOverlay");
+            console.log(haveOverlay);
+            console.log(isMuted);
+            playerid.removeChild(divOverlay);
+            player.muted(false);
+            isMuted = false;
+          }
+
+        });
+
+        console.log("isMuted: " + isMuted);
+        console.log("haveOverlay: " + haveOverlay)
+        // IF isMuted=false BUTON OVERLAY WORKED, REMOVE OVERLAY AND DO NOT CONTINUE RETURN FALSE
+        if (!isMuted) {
+
+          if (haveOverlay) {
+            playerid.removeChild(divOverlay);
+            haveOverlay = false;
+          }
+
           return false;
           // alert("true");
         } else {
           // isUnmuted = false;
-          // hasPlayed = true;
-          hasOverlay = true;
+          // haveOverlay = true;
+          //haveOverlay = true;
         }
+
         // var styleElem = document.head.appendChild(
         //   document.createElement("style")
         // );
         // styleElem.innerHTML = '.vjs-unmute:before {font-size: 1.3em; margin-right: 8px;line-height: 1.67;font-family: VideoJS;content: "\f104";vertical-align: middle;}';
+
         // +++ Configure the button +++
         // div.textContent = "Unmute";
         divOverlay.classList.add("vjs-unmute-overlay");
         divOverlay.setAttribute("style", "z-index: 1");
+
         // const playerid = document.getElementById("videoid");
         // var unmutedbutton = document.querySelector('#'+playerid+'.vjs-unmute-overlay');
+
         //   button.setAttribute(
         //   "style",
         //   "font-size:16px; color:white; font-weight:bold; background-color:black; width:100px; height:30px; opacity: .65;"
         // );
+
         // +++ Add the button to the container +++
         // const playerid = document.getElementById("videoid");
         //const playerid = myPlayer.id();
         playerid.appendChild(divOverlay);
+
         let unmutedbutton = document.createElement("span");
         let getoverlay = document
           .getElementById(thisplayerid)
@@ -154,6 +297,7 @@ registerPlugin("unmuteButton", function() {
         unmutedbutton.textContent = translated;
         unmutedbutton.classList.add("vjs-unmute");
         unmutedbutton.setAttribute("style", "z-index: 1");
+
         getoverlay.appendChild(unmutedbutton);
       });
       // .catch((error) => {
@@ -164,16 +308,8 @@ registerPlugin("unmuteButton", function() {
       //     // Handle a load or playback error
       //   }
       // });
+
     }
-    startPlayPromise
-      .then(function() {
-        // alert('CONSOLE2: Autoplay started!')
-        console.log("CONSOLE2: Autoplay started!");
-        // Autoplay started!
-      })
-      .catch(function(error) {
-        console.log("CONSOLE2 Autoplay was prevented!");
-        // Autoplay was prevented.
-      });
+
   });
 });
