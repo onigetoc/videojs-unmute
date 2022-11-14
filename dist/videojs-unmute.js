631d55795a9d15b5683e05d7029f93dbc7fa2b2f
Gino <ginocote@gmail.com>
2022-11-14 12:14:12 -0500

    No more touch

--- dist/videojs-unmute.js
+++ dist/videojs-unmute.js
@@ -119,59 +119,59 @@ registerPlugin("unmuteButton", function () {
 /****************** DETECTING USER EVENT ******************/
 
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  console.log('getTouches');
-  player.muted(false);
-          isMuted = false;
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-  console.log('handleTouchStart');
-};                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-    console.log('handleTouchMove');
-        return;
-    }
-  
-            player.muted(false);
-          isMuted = false;
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */ 
-        } else {
-            /* left swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+//document.addEventListener('touchstart', handleTouchStart, false);        
+//document.addEventListener('touchmove', handleTouchMove, false);
+//
+//var xDown = null;                                                        
+//var yDown = null;
+//
+//function getTouches(evt) {
+//  console.log('getTouches');
+//  player.muted(false);
+//          isMuted = false;
+//  return evt.touches ||             // browser API
+//         evt.originalEvent.touches; // jQuery
+//}                                                     
+//                                                                         
+//function handleTouchStart(evt) {
+//    const firstTouch = getTouches(evt)[0];                                      
+//    xDown = firstTouch.clientX;                                      
+//    yDown = firstTouch.clientY;                                      
+//  console.log('handleTouchStart');
+//};                                                
+//                                                                         
+//function handleTouchMove(evt) {
+//    if ( ! xDown || ! yDown ) {
+//    console.log('handleTouchMove');
+//        return;
+//    }
+//  
+//            player.muted(false);
+//          isMuted = false;
+//
+//    var xUp = evt.touches[0].clientX;                                    
+//    var yUp = evt.touches[0].clientY;
+//
+//    var xDiff = xDown - xUp;
+//    var yDiff = yDown - yUp;
+//                                                                         
+//    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//        if ( xDiff > 0 ) {
+//            /* right swipe */ 
+//        } else {
+//            /* left swipe */
+//        }                       
+//    } else {
+//        if ( yDiff > 0 ) {
+//            /* down swipe */ 
+//        } else { 
+//            /* up swipe */
+//        }                                                                 
+//    }
+//    /* reset values */
+//    xDown = null;
+//    yDown = null;                                             
+//};
 
 
 /****************** DETECTING USER EVENTS ******************/
